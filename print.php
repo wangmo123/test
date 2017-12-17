9dfb2af69c3152ef6a419ee08abbbde13c3f7741
aabbcc <llllkkkk102030@163.com>
2017-12-17 10:56:51 +0800

    Update print.php

--- print.php
+++ print.php
@@ -1,45 +1,32 @@
 <?php
-
-	function printfile($printer = "Microsoft Print to PDF", $file = array('D:/test/word/33.pdf'), $num = 1)
-	{
-		$print_str = 'soffice --headless --pt "'.$printer.'" ';
-		if (is_array($file)) {
-			$new_file = array();
-			for ($i = 0; $i < $num; $i++) {
-				$new_file = array_merge($new_file, $file);
-			}
-		
-			foreach ($new_file as $key => $value) {
-				if (!file_exists($value)) {
-					continue;
-				}
-				$print_str .= $value.' ';
-			}
-	
-		
-			echo pclose(popen("start /B ". $print_str, "r"));  
-		} else {
-			if (!file_exists($file)) {
-				return -1;
-			}
-			$print_str .= $file;
-			exec($print_str.' 2>&1');
+/**
+ * 批量打印文件
+ * @param printer 打印机名称，未传值或者传值错误时，将启用默认打印机
+ * @param file   字符串或数组，非法字符过滤，自己过滤 （= =）
+ * @param num  file里的文件要打印的份数
+ */
+function printfile($printer = "Microsoft Print to PDF", $file = array('D:/test/word/33.pdf'), $num = 1)
+{
+	$print_str = 'soffice --headless --pt "'.$printer.'" ';
+	if (is_array($file)) {
+		$new_file = array();
+		for ($i = 0; $i < $num; $i++) {
+			$new_file = array_merge($new_file, $file);
 		}
-	}
-	echo1("start");
-	printfile();
-	echo1("over");
-	echo1(php_uname());
 	
-	function echo1($str)
-	{
-		echo $str;
-		echo "<br>";
+		foreach ($new_file as $key => $value) {
+			if (!file_exists($value)) {
+				continue;
+			}
+			$print_str .= $value.' ';
+		}
+		echo pclose(popen("start /B ". $print_str, "r"));  //此方式避免php程序挂起，后台执行打印
+	} else {
+		if (!file_exists($file)) {
+			return -1;
+		}
+		$print_str .= $file;
+		echo pclose(popen("start /B ". $print_str, "r"));  
 	}
-	
-	function var_dump1($info)
-	{
-		echo "<pre>";
-		var_dump($info);
-		echo "</pre>";
-	}
+}
+
